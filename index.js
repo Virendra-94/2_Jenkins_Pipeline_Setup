f38a2ca41456b58f27fd5adad75383d8c68a1825
Virendra-94 <virendrakumarofficial94@gmail.com>
2025-04-26 13:56:57 +0530

    Node and Docker Files added

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => res.send('Hello from Dockerized Node.js App by Virendra Kumar!'));
+app.listen(PORT, () => console.log(`Running on port ${PORT}`));
